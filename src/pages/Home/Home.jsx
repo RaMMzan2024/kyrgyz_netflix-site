5fef7fe61de8e7f8c04fca4afe60089dabf462cd
Ramazan <148753896+RaMMzan2024@users.noreply.github.com>
2024-05-15 14:40:59 +0600

    commits

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Home.css';
-import Navbar from '../../components/Navbar/Navbar'; 
+import Navbar from '../../components/Navbar/Navbar';
 import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
@@ -14,28 +14,28 @@ import Footer from '../../components/Footer/Footer';
 const Home = () => {
   return (
     <div className='home'>
-      <Navbar/>
+      <Navbar />
       <div className="hero">
-        <img src={hero_banner} alt="" className='banner-img'/>
+        <img src={hero_banner} alt="" className='banner-img' />
         <div className="hero-caption">
-          <img src={hero_title} alt="" className='caption-img'/>
+          <img src={hero_title} alt="" className='caption-img' />
           <p>Discovering his ties to secret ancient order, a young
             man living a modern Istanbul embarks on a quest to save the
             city from an immortal enemy.</p>
-            <div className="hero-btns">
-              <button className='btn'><img src={play_icon} alt="" />Play</button>
-              <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
-            </div>
-            <TitileCards/>
+          <div className="hero-btns">
+            <button className='btn'><img src={play_icon} alt="" />Play</button>
+            <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
+          </div>
+          <TitileCards />
         </div>
       </div>
       <div className="more-cards">
-      <TitileCards title={"Blockbuster Movies"} category={"top_rated"}/>
-      <TitileCards title={"Only on Netflix"} category={"popular"}/>
-      <TitileCards title={"Upcomiing"} category={"upcoming"}/>
-      <TitileCards title={"Top Pics for You"} category={"now_playing"}/>
+        <TitileCards title={"Blockbuster Movies"} category={"top_rated"} />
+        <TitileCards title={"Only on Netflix"} category={"popular"} />
+        <TitileCards title={"Upcomiing"} category={"upcoming"} />
+        <TitileCards title={"Top Pics for You"} category={"now_playing"} />
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
